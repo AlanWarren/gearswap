4f78874c3fb391851cdae22b36423b3942abfb9f
Alan Warren <bluemoonshine@gmail.com>
2014-05-03 17:12:09 -0500

    adding back Kheper as regen

--- NIN.lua
+++ NIN.lua
@@ -209,7 +209,7 @@ function init_gear_sets()
         neck="Twilight Torque",
         ear1="Dudgeon Earring",
         ear2="Heartseeker Earring",
-		body="War Shinobi Gi",
+		body="Kheper Jacket",
         hands="Mochizuki Tekko +1",
         ring1="Dark Ring",
         ring2="Paguroidea Ring",
