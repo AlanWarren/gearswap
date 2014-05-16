454e466747a8297265450a8a752842835146f948
Alan Warren <alanw@televox.com>
2014-05-16 10:40:37 -0500

    adding Nuking functionality to GEO

--- GEO.lua
+++ GEO.lua
@@ -1,6 +1,7 @@
 -------------------------------------------------------------------------------------------------------------------
 -- Initialization function that defines sets and variables to be used.
 -------------------------------------------------------------------------------------------------------------------
+-- NOTE: This is Mote's GEO.lua with added support for nuking like a boss
 
 -- IMPORTANT: Make sure to also get the Mote-Include.lua file (and its supplementary files) to go with this.
 
@@ -18,6 +19,7 @@ end
 -- Setup vars that are user-dependent.  Can override this function in a sidecar file.
 function user_setup()
 	-- Options: Override default values
+    options.CastingModes = {'Normal', 'Resistant', 'Proc'}
 	options.OffenseModes = {'Normal'}
 	options.DefenseModes = {'Normal'}
 	options.WeaponskillModes = {'Normal'}
@@ -27,6 +29,7 @@ function user_setup()
 	options.MagicalDefenseModes = {'MDT'}
 
 	state.Defense.PhysicalMode = 'PDT'
+    state.OffenseMode = 'None'
 	
 	-- Default macro set/book
 	set_macro_page(1, 6)
@@ -62,7 +65,12 @@ function init_gear_sets()
 		body="Heka's Kalasiris",ring1="Prolix Ring",
 		back="Pahtli Cape",waist="Witful Belt",legs="Orvail Pants +1",feet="Chelona Boots +1"}
 
-       
+    sets.precast.FC.Curaga = sets.precast.FC.Cure
+
+    sets.precast.FC['Enhancing Magic'] = set_combine(sets.precast.FC, {})
+
+    sets.precast.FC['Elemental Magic'] = set_combine(sets.precast.FC, {neck="Stoicheion Medal"}) 
+
 	-- Weaponskill sets
 	-- Default set for any weaponskill that isn't any more specifically defined
 
@@ -84,7 +92,8 @@ function init_gear_sets()
 		body="Hagondes Coat",hands="Bokwus Gloves",ring1="Prolix Ring",
 		back="Swith Cape",waist="Goading Belt",legs="Hagondes Pants",feet="Hagondes Sabots"}
 		
-	sets.midcast.Geomancy = {range="Matre Bell"}
+	sets.midcast.Geomancy = {range="Filiae Bell", body="Bagua Tunic", hands="Geomancy Mitaines",
+        back="Lifestream Cape", legs="Bagua Pants"}
 
 	sets.midcast.Cure = {
 		body="Heka's Kalasiris",hands="Bokwus Gloves",
@@ -94,7 +103,56 @@ function init_gear_sets()
 
 	sets.midcast.Shellra = {ring1="Sheltered Ring"}
 
-	
+	sets.midcast['Enhancing Magic'] = { sub="Fulcio Grip", neck="Colossus's Torque", body="Anhur Robe",
+        hands="Ayao's Gloves"
+    }
+    sets.midcast.Stoneskin = sets.midcast['Enhancing Magic']
+
+    sets.midcast.Macc = { main="Antinian Staff", sub="Mephitis Grip", ammo="Aureole",
+        head="Nahtirah Hat", neck="Eddy Necklace", lear="Lifestorm Earring", rear="Psystorm Earring",
+        body="Bokwus Robe", hands="Hagondes Cuffs", lring="Balrahn's Ring", rring="Sangoma Ring",
+        back="Lifestream Cape", waist="Demonry Sash", legs="Bokwus Slops", feet="Bokwus Boots"}
+    
+    sets.midcast['Enfeebling Magic'] = sets.midcast.Macc
+    sets.midcast.ElementalEnfeeble = sets.midcast.Macc
+
+    sets.midcast['Dark Magic']  = set_combine(sets.midcast.Macc, {
+        head="Bagua Galero", neck="Aesir Torque",
+        body="Geomancy Tunic", back="Merciful Cape",
+        waist="Fucho-no-Obi"
+    })
+    sets.midcast.Aspir = set_combine(sets.midcast['Dark Magic'], { lring="Excelsis Ring" })
+    sets.midcast.Drain = sets.midcast.Aspir
+    sets.midcast.Stun = sets.midcast.Macc
+
+	-- Elemental Magic sets are default for handling low-tier nukes.
+    sets.midcast.LowTierNuke = {main="Lehbrailg +2",sub="Wizzan Grip",ammo="Witchstone",
+        head="Hagondes Hat",neck="Stoicheion Medal",ear1="Hecate's Earring",ear2="Friomisi Earring",
+        body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Icesoul Ring",ring2="Strendu Ring",
+        back="Toro Cape",waist=gear.ElementalObi,legs="Hagondes Pants",feet="Hagondes Sabots"}
+    
+    sets.midcast.LowTierNuke.Resistant = set_combine(sets.midcast.LowTierNuke, {
+        ear1="Psystorm Earring", ear2="Lifestorm Earring",
+        feet="Bokwus Boots"
+    })
+    
+    -- Idle set when doing procs. Fast cast gear, minimal nuke gear. Won't change out of this for nukes.
+    sets.midcast.LowTierNuke.Proc = {main="Earth Staff", sub="Wizzan Grip",ammo="Impatiens",
+        head="Nahtirah Hat",neck="Twilight Torque",ear1="Bloodgem Earring",ear2="Loquacious Earring",
+        body="Manasa Chasuble",hands="Serpentes Cuffs",ring1="Sheltered Ring",ring2="Paguroidea Ring",
+        back="Swith Cape",waist="Witful Belt",legs="Nares Trews",feet="Chelona Boots +1"}
+    
+    -- Custom classes for high-tier nukes.
+    sets.midcast.HighTierNuke = {main="Lehbrailg +2",sub="Wizzan Grip",ammo="Witchstone",
+        head="Hagondes Hat",neck="Stoicheion Medal",ear1="Hecate's Earring",ear2="Friomisi Earring",
+        body="Hagondes Coat",hands="Yaoyotl Gloves",ring1="Icesoul Ring",ring2="Strendu Ring",
+        back="Toro Cape",waist=gear.ElementalObi,legs="Hagondes Pants",feet="Hagondes Sabots"}
+    
+    sets.midcast.HighTierNuke.Resistant = set_combine(sets.midcast.HighTierNuke, {
+        ear1="Psystorm Earring", ear2="Lifestorm Earring",
+        feet="Bokwus Boots"
+    })
+
 	-- Sets to return to when not performing an action.
 	
 	-- Resting sets
@@ -138,6 +196,12 @@ function init_gear_sets()
 		back="Umbra Cape",waist="Goading Belt",legs="Bokwus Slops",feet="Hagondes Sabots"}
 
 	sets.Kiting = {feet="Herald's Gaiters"}
+	
+    lowTierNukes = S{'Stone', 'Water', 'Aero', 'Fire', 'Blizzard', 'Thunder',
+        'Stone II', 'Water II', 'Aero II', 'Fire II', 'Blizzard II', 'Thunder II',
+        'Stone III', 'Water III', 'Aero III', 'Fire III', 'Blizzard III', 'Thunder III',
+        'Stonega', 'Waterga', 'Aeroga', 'Firaga', 'Blizzaga', 'Thundaga',
+        'Stonega II', 'Waterga II', 'Aeroga II', 'Firaga II', 'Blizzaga II', 'Thundaga II'}
 
 	-- Engaged sets
 
@@ -160,7 +224,11 @@ end
 -- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
 -- Set eventArgs.useMidcastGear to true if we want midcast gear equipped on precast.
 function job_precast(spell, action, spellMap, eventArgs)
-
+	if spellMap == 'Cure' or spellMap == 'Curaga' then
+        gear.default.obi_waist = "Goading Belt"
+    elseif spell.skill == 'Elemental Magic' then
+        gear.default.obi_waist = "Sekhmet Corset"
+    end
 end
 
 -- Run after the general precast() is done.
@@ -174,6 +242,11 @@ function job_midcast(spell, action, spellMap, eventArgs)
 	if spell.action_type == 'Magic' then
 		-- Default base equipment layer of fast recast.
 		equip(sets.midcast.FastRecast)
+
+        if spell.skill == 'Elemental Magic' and state.CastingMode == 'Proc' then
+            add_to_chat(15, 'Proc mode, no damage gear for midcast.')
+            eventArgs.handled = true
+        end
 	end
 end
 
@@ -191,6 +264,16 @@ end
 -------------------------------------------------------------------------------------------------------------------
 -- Customization hooks for idle and melee sets, after they've been automatically constructed.
 -------------------------------------------------------------------------------------------------------------------
+-- Custom spell mapping.
+function job_get_spell_map(spell, default_spell_map)
+    if spell.skill == 'Elemental Magic' and default_spell_map ~= 'ElementalEnfeeble' then
+        if lowTierNukes:contains(spell.english) then
+            return 'LowTierNuke'
+        else
+            return 'HighTierNuke'
+        end
+    end
+end
 
 function customize_idle_set(idleSet)
 	return idleSet
@@ -216,7 +299,20 @@ function job_buff_change(buff, gain)
 	--handle_equipping_gear(player.status)
 end
 
-
+-- Handle notifications of general user state change.
+function job_state_change(stateField, newValue)
+    if stateField == 'OffenseMode' then
+        if newValue == 'Normal' then
+            disable('main','sub')
+        else
+            enable('main','sub')
+        end
+    elseif stateField == 'Reset' then
+        if state.OffenseMode == 'None' then
+            enable('main','sub')
+        end
+    end
+end
 -------------------------------------------------------------------------------------------------------------------
 -- User code that supplements self-commands.
 -------------------------------------------------------------------------------------------------------------------
@@ -233,8 +329,26 @@ end
 
 -- Function to display the current relevant user state when doing an update.
 -- Return true if display was handled, and you don't want the default info shown.
-function display_current_job_state()
-
+function display_current_job_state(eventArgs)
+    local meleeString = ''
+    if state.OffenseMode == 'Normal' then
+        meleeString = 'Melee: Weapons locked, '
+    end
+    
+    local defenseString = ''
+    if state.Defense.Active then
+        local defMode = state.Defense.PhysicalMode
+        if state.Defense.Type == 'Magical' then
+            defMode = state.Defense.MagicalMode
+        end
+    
+        defenseString = 'Defense: '..state.Defense.Type..' '..defMode..', '
+    end
+    
+    add_to_chat(122,meleeString..'Casting ['..state.CastingMode..'], Idle ['..state.IdleMode..'], '..defenseString..
+    'Kiting: '..on_off_names[state.Kiting])
+    
+    eventArgs.handled = true
 end
 
 -------------------------------------------------------------------------------------------------------------------
