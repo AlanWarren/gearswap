a3541da143eb2db26b8203022326efa1b215fb57
Alan Warren <bluemoonshine@gmail.com>
2014-05-31 14:35:32 -0500

    adding a conditional to enable counter

--- NIN.lua
+++ NIN.lua
@@ -639,6 +639,8 @@ function job_status_change(newStatus, oldStatus, eventArgs)
     if utsusemi_active() then
         add_to_chat(8, 'Counter Mode Disabled!')
         state.Buff.Yonin = false
+    elseif buffactive.Yonin then
+        state.Buff.Yonin = true
     end
 end
 
