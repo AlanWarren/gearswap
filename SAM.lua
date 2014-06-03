5f01b914264189ede508a0cf235ea355addc171b
Alan Warren <alanw@televox.com>
2014-06-03 09:46:32 -0500

    removing comment

--- SAM.lua
+++ SAM.lua
@@ -692,21 +692,6 @@ function determine_bow()
     	classes.CustomMeleeGroups:append('Yoichi')
     end
 end
--- will try and bend this to work for cor rolls. I'd like to know the value of sam roll
---windower.register_event('incoming text', function(original, new, color)
---    local name, count = original:match('Additional effect: Treasure Hunter effectiveness against the (.*) increases to (%d+).')
---    if name and count then
---        mob = name
---        th:text(' '..name..'\n TH: '..count);
---        th:show()
---    end
---
---    name = original:match('%w+ defeats the (.*).')
---    if name and mob == name then
---        th:text('')
---        th:hide()
---    end
---end)
 
 -- Select default macro book on initial load or subjob change.
 function select_default_macro_book()
