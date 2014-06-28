81aa36a4ceaa099aa8989e09e396330c0a81aa30
Alan Warren <bluemoonshine@gmail.com>
2014-06-28 12:07:35 -0500

    adding a PDT iLvl set

--- THF.lua
+++ THF.lua
@@ -412,6 +412,10 @@ function init_gear_sets()
 	sets.engaged.iLvl = set_combine(sets.engaged, {
 		body="Qaaxo Harness",
     })
+    sets.engaged.iLvl.PDT = set_combine(sets.engaged.iLvl, {
+        head="Lithelimb Cap",
+        ring1="Patricius Ring"
+    })
 	sets.engaged.Evasion = set_combine(sets.engaged, {
 		body="Qaaxo Harness",
         ring1="Patricius Ring",
