905855a5f588f30160ec6f099e53c2433569afe4
Alan Warren <bluemoonshine@gmail.com>
2014-07-21 22:11:30 -0500

    adding ammo to idle

--- DRK.lua
+++ DRK.lua
@@ -263,6 +263,7 @@
             }
            
             sets.idle.Field = {
+                ammo="Yetshila",
                 head="Twilight Helm",
                 neck="Bale Choker",
                 ear1="Bladeborn Earring",
