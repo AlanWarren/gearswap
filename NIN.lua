f0d5581a22f2b43840e76a3c674be403db655148
Alan Warren <bluemoonshine@gmail.com>
2014-08-08 13:18:19 -0500

    adding back iga hands

--- NIN.lua
+++ NIN.lua
@@ -165,7 +165,7 @@ function init_gear_sets()
         ear2="Crematio Earring",
         neck="Stoicheion Medal",
     	body="Mochizuki Chainmail +1",
-        hands="Mochizuki Tekko +1",
+        hands="Iga Tekko +2",
     	back="Toro Cape",
         ring1="Sangoma Ring",
         ring2="Acumen Ring",
