52a3f2cb12a4c729e73d70cdd93edcc708e92952
Alan Warren <bluemoonshine@gmail.com>
2014-08-22 22:45:26 -0500

    editing Hi set

--- NIN.lua
+++ NIN.lua
@@ -532,7 +532,7 @@ function init_gear_sets()
         back="Rancorous Mantle",
         legs="Otronif Brais +1",
         waist="Windbuffet Belt",
-        feet="Otronif Boots +1"
+        feet="Mochizuki Kyahan +1"
     }
     sets.precast.WS['Blade: Hi'] = set_combine(sets.precast.WS, sets.Hi)
     sets.precast.WS['Blade: Hi'].Mid = set_combine(sets.precast.WS['Blade: Hi'], {
