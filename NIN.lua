d5043a431a671d9777be34cf369259eca1b9e851
Alan Warren <bluemoonshine@gmail.com>
2014-08-30 10:40:29 -0500

    removed fc from uxkaj

--- NIN.lua
+++ NIN.lua
@@ -118,7 +118,7 @@ function init_gear_sets()
     
     -- Fast cast sets for spells
     sets.precast.FC = {
-        head="Uk'uxkaj Cap",
+        --head="Uk'uxkaj Cap",
         ear1="Loquacious Earring",
         ring1="Prolix Ring",
         hands="Buremte Gloves",
