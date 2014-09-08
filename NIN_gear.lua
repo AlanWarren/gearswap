7c2d3ebd9cc035218e504f11c3fc7b123404cc9b
Alan Warren <alanw@televox.com>
2014-09-08 17:04:55 -0500

    fixing tabs

--- NIN_gear.lua
+++ NIN_gear.lua
@@ -543,8 +543,8 @@ function init_gear_sets()
         waist="Thunder Belt",
         feet="Mochizuki Kyahan +1"
      })
-sets.precast.WS['Blade: Chi'] = sets.precast.WS['Aeolian Edge']
-sets.precast.WS['Blade: To'] = sets.precast.WS['Aeolian Edge']
+    sets.precast.WS['Blade: Chi'] = sets.precast.WS['Aeolian Edge']
+    sets.precast.WS['Blade: To'] = sets.precast.WS['Aeolian Edge']
 
 end
 
