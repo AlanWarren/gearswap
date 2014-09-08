30fa845094b83508099195b6b00676b54d60a695
Alan Warren <bluemoonshine@gmail.com>
2014-09-07 20:50:55 -0500

    editing sets

--- SAM.lua
+++ SAM.lua
@@ -162,7 +162,8 @@ function init_gear_sets()
     sets.precast.WS['Tachi: Shoha'] = set_combine(sets.precast.WS, {
         neck="Breeze Gorget",
         feet="Ejekamal Boots",
-        legs="Otronif Brais +1"
+        legs="Otronif Brais +1",
+        waist="Thunder Belt"
     })
     sets.precast.WS['Tachi: Shoha'].Mid = set_combine(sets.precast.WS.Acc, {
         head="Yaoyotl Helm",
