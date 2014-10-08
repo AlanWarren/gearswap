bb25a74aaf43a4ed9ce4cbdee752d37dc562296b
Alan Warren <alanw@televox.com>
2014-10-08 16:05:12 -0500

    removing unused sets

--- NIN_gear.lua
+++ NIN_gear.lua
@@ -258,12 +258,6 @@ function init_gear_sets()
         feet="Otronif Boots +1"
     })
     
-    sets.engaged.Haste_40 = {}
-    sets.engaged.Haste_35 = {}
-    sets.engaged.Haste_30 = {}
-    sets.engaged.Haste_25 = {}
-    sets.engaged.Haste_20 = {}
-
     sets.engaged.HastePDT = {
         neck="Agitator's Collar",
         hands="Otronif Gloves +1",
