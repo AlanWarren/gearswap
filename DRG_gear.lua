4a0f808d77a368d9384733e6ef60fa38d4bf9507
Alan Warren <bluemoonshine@gmail.com>
2015-03-29 01:02:08 -0500

    adding limbo

--- DRG_gear.lua
+++ DRG_gear.lua
@@ -79,6 +79,7 @@ function init_gear_sets()
         head="Cizin Helm +1", 
         ear1="Loquacious Earring", 
         hands="Buremte Gloves",
+        legs="Limbo Trousers",
         ring1="Prolix Ring"
     }
     
