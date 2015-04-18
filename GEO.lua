4b48f147fb910e2c2a4316a07329e584198969f9
Alan Warren <bluemoonshine@gmail.com>
2015-04-18 16:03:04 -0500

    editing enhanc3

--- GEO.lua
+++ GEO.lua
@@ -237,7 +237,7 @@ function init_gear_sets()
         feet="Bagua Sandals"
     })
 
-	sets.midcast['Enhancing Magic'] = sets.midcast.Macc
+	sets.midcast['Enhancing Magic'] = sets.midcast.FastRecast
         --sub="Fulcio Grip", 
         --neck="Colossus's Torque", 
         --body="Anhur Robe",
