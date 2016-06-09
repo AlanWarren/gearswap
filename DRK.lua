08388e845c4ad88e4200180f6fb6976ef0288151
Alan Warren <bluemoonshine@gmail.com>
2016-06-09 18:34:20 -0500

    editing sets

--- DRK.lua
+++ DRK.lua
@@ -882,7 +882,7 @@ function init_gear_sets()
 
     -- great sword
     sets.engaged.GreatSword = set_combine(sets.engaged, {
-        head="Otomi Helm",
+        head="Valorous Helm",
         ear1="Brutal Earring",
         ear2="Tripudio Earring"
     })
